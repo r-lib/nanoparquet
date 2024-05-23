ace4881afaf69bfe22bf17898907e33cf95aa612
Gábor Csárdi <csardi.gabor@gmail.com>
2024-05-23 14:47:14 +0200

    R: implement partial column writes
    They are not used from the lib yet.

--- src/write.cpp
+++ src/write.cpp
@@ -66,31 +66,36 @@ RParquetOutFile::RParquetOutFile(
 void RParquetOutFile::write_int32(std::ostream &file, uint32_t idx,
                                   uint64_t from, uint64_t until) {
   SEXP col = VECTOR_ELT(df, idx);
-  R_xlen_t len = XLENGTH(col);
-  file.write((const char *)INTEGER(col), sizeof(int) * len);
+  if (until > Rf_xlength(col)) {
+    Rf_error("Internal nanoparquet error, row index too large");
+  }
+  uint64_t len = until - from;
+  file.write((const char *) (INTEGER(col) + from), sizeof(int) * len);
 }
 
 void RParquetOutFile::write_int64(std::ostream &file, uint32_t idx,
                                   uint64_t from, uint64_t until) {
   // This is double in R, so we need to convert
   SEXP col = VECTOR_ELT(df, idx);
-  R_xlen_t len = Rf_xlength(col);
+  if (until > Rf_xlength(col)) {
+    Rf_error("Internal nanoparquet error, row index too large");
+  }
   if (Rf_inherits(col, "POSIXct")) {
     // need to convert seconds to microseconds
-    for (R_xlen_t i = 0; i < len; i++) {
+    for (uint64_t i = from; i < until; i++) {
       int64_t el = REAL(col)[i] * 1000 * 1000;
       file.write((const char*) &el, sizeof(int64_t));
     }
 
   } else if (Rf_inherits(col, "difftime")) {
     // need to convert seconds to nanoseconds
-    for (R_xlen_t i = 0; i < len; i++) {
+    for (uint64_t i = from; i < until; i++) {
       int64_t el = REAL(col)[i] * 1000 * 1000 * 1000;
       file.write((const char*) &el, sizeof(int64_t));
     }
 
   } else {
-    for (R_xlen_t i = 0; i < len; i++) {
+    for (uint64_t i = from; i < until; i++) {
       int64_t el = REAL(col)[i];
       file.write((const char*) &el, sizeof(int64_t));
     }
@@ -100,15 +105,20 @@ void RParquetOutFile::write_int64(std::ostream &file, uint32_t idx,
 void RParquetOutFile::write_double(std::ostream &file, uint32_t idx,
                                    uint64_t from, uint64_t until) {
   SEXP col = VECTOR_ELT(df, idx);
-  R_xlen_t len = XLENGTH(col);
-  file.write((const char *)REAL(col), sizeof(double) * len);
+  if (until > Rf_xlength(col)) {
+    Rf_error("Internal nanoparquet error, row index too large");
+  }
+  uint64_t len = until - from;
+  file.write((const char *) (REAL(col) + from), sizeof(double) * len);
 }
 
 void RParquetOutFile::write_byte_array(std::ostream &file, uint32_t idx,
                                        uint64_t from, uint64_t until) {
   SEXP col = VECTOR_ELT(df, idx);
-  R_xlen_t len = XLENGTH(col);
-  for (R_xlen_t i = 0; i < len; i++) {
+  if (until > Rf_xlength(col)) {
+    Rf_error("Internal nanoparquet error, row index too large");
+  }
+  for (uint64_t i = from; i < until; i++) {
     const char *c = CHAR(STRING_ELT(col, i));
     uint32_t len1 = strlen(c);
     file.write((const char *)&len1, 4);
@@ -123,9 +133,11 @@ uint32_t RParquetOutFile::get_size_byte_array(
   uint64_t until) {
 
   SEXP col = VECTOR_ELT(df, idx);
-  R_xlen_t len = Rf_xlength(col);
+  if (until > Rf_xlength(col)) {
+    Rf_error("Internal nanoparquet error, row index too large");
+  }
   uint32_t size = 0;
-  for (R_xlen_t i = 0; i < len; i++) {
+  for (uint64_t i = from; i < until; i++) {
     SEXP csxp = STRING_ELT(col, i);
     if (csxp != NA_STRING) {
       const char *c = CHAR(csxp);
@@ -135,9 +147,13 @@ uint32_t RParquetOutFile::get_size_byte_array(
   return size;
 }
 
-void write_boolean_impl(std::ostream &file, SEXP col) {
-  R_xlen_t len = XLENGTH(col);
-  int *p = LOGICAL(col);
+void write_boolean_impl(std::ostream &file, SEXP col,
+                        uint64_t from, uint64_t until) {
+  if (until > Rf_xlength(col)) {
+    Rf_error("Internal nanoparquet error, row index too large");
+  }
+  uint64_t len = until - from;
+  int *p = LOGICAL(col) + from;
   int *end = p + len;
   while (p + 8 < end) {
     char b = 0;
@@ -163,14 +179,17 @@ void write_boolean_impl(std::ostream &file, SEXP col) {
 void RParquetOutFile::write_boolean(std::ostream &file, uint32_t idx,
                                     uint64_t from, uint64_t until) {
   SEXP col = VECTOR_ELT(df, idx);
-  write_boolean_impl(file, col);
+  write_boolean_impl(file, col, from, until);
 }
 
 uint32_t RParquetOutFile:: write_present(std::ostream &file, uint32_t idx,
                                          uint64_t from, uint64_t until) {
   SEXP col = VECTOR_ELT(df, idx);
-  R_xlen_t len = XLENGTH(col);
-  R_xlen_t num_pres = 0;
+  if (until > Rf_xlength(col)) {
+    Rf_error("Internal nanoparquet error, row index too large");
+  }
+  uint64_t len = until - from;
+  uint64_t num_pres = 0;
   present.reset(len * sizeof(int));
   int *presptr = (int*) present.ptr;
   switch(TYPEOF(col)) {
@@ -225,8 +244,10 @@ void RParquetOutFile::write_present_int32(
   uint64_t until) {
 
   SEXP col = VECTOR_ELT(df, idx);
-  R_xlen_t len = XLENGTH(col);
-  for (auto i = 0; i < len; i++) {
+  if (until > Rf_xlength(col)) {
+    Rf_error("Internal nanoparquet error, row index too large");
+  }
+  for (uint64_t i = from; i < until; i++) {
     int el = INTEGER(col)[i];
     if (el != NA_INTEGER) {
       file.write((const char*) &el, sizeof(int));
@@ -242,9 +263,11 @@ void RParquetOutFile::write_present_int64(
   uint64_t until) {
 
   SEXP col = VECTOR_ELT(df, idx);
-  R_xlen_t len = Rf_xlength(col);
+  if (until > Rf_xlength(col)) {
+    Rf_error("Internal nanoparquet error, row index too large");
+  }
   if (Rf_inherits(col, "POSIXct")) {
-    for (R_xlen_t i = 0; i < len; i++) {
+    for (uint64_t i = from; i < until; i++) {
       double el = REAL(col)[i];
       if (R_IsNA(el)) continue;
       int64_t el2 = el * 1000 * 1000;
@@ -252,7 +275,7 @@ void RParquetOutFile::write_present_int64(
     }
   } else if (Rf_inherits(col, "difftime")) {
     // need to convert seconds to nanoseconds
-    for (R_xlen_t i = 0; i < len; i++) {
+    for (uint64_t i = from; i < until; i++) {
       double el = REAL(col)[i];
       if (R_IsNA(el)) continue;
       int64_t el2 = el * 1000 * 1000 * 1000;
@@ -260,7 +283,7 @@ void RParquetOutFile::write_present_int64(
     }
 
   } else {
-    for (R_xlen_t i = 0; i < len; i++) {
+    for (uint64_t i = from; i < until; i++) {
       double el = REAL(col)[i];
       if (R_IsNA(el)) continue;
       int64_t el2 = el;
@@ -294,8 +317,10 @@ void RParquetOutFile::write_present_byte_array(
   uint64_t until) {
 
   SEXP col = VECTOR_ELT(df, idx);
-  R_xlen_t len = XLENGTH(col);
-  for (R_xlen_t i = 0; i < len; i++) {
+  if (until > Rf_xlength(col)) {
+    Rf_error("Internal nanoparquet error, row index too large");
+  }
+  for (uint64_t i = from; i < until; i++) {
     SEXP csxp = STRING_ELT(col, i);
     if (csxp != R_NaString) {
       const char *c = CHAR(csxp);
@@ -315,14 +340,16 @@ void RParquetOutFile::write_present_boolean(
 
   SEXP col = VECTOR_ELT(df, idx);
   SEXP col2 = PROTECT(Rf_allocVector(LGLSXP, num_present));
-  R_xlen_t i, o, len = XLENGTH(col);
-
-  for (i = 0, o = 0; i < len; i++) {
+  uint64_t i, o;
+  if (until > Rf_xlength(col)) {
+    Rf_error("Internal nanoparquet error, row index too large");
+  }
+  for (i = from, o = 0; i < until; i++) {
     if (LOGICAL(col)[i] != NA_LOGICAL) {
       LOGICAL(col2)[o++] = LOGICAL(col)[i];
     }
   }
-  write_boolean_impl(file, col2);
+  write_boolean_impl(file, col2, 0, num_present);
 
   UNPROTECT(1);
 }
@@ -368,8 +395,10 @@ void RParquetOutFile::write_dictionary_indices(
   uint64_t until) {
 
   SEXP col = VECTOR_ELT(df, idx);
-  R_xlen_t len = Rf_xlength(col);
-  for (R_xlen_t i = 0; i < len; i++) {
+  if (until > Rf_xlength(col)) {
+    Rf_error("Internal nanoparquet error, row index too large");
+  }
+  for (uint64_t i = from; i < until; i++) {
     int el = INTEGER(col)[i];
     if (el != NA_INTEGER) {
       el--;
