06e9f5b5389a93d01cb6af1376fff4a887dbfbf1
Gábor Csárdi <csardi.gabor@gmail.com>
2024-06-06 21:53:47 +0200

    Improve DBP decoder code
    - better variable names
    - sanity checks

--- src/lib/DbpDecoder.h
+++ src/lib/DbpDecoder.h
@@ -12,36 +12,63 @@ class DbpDecoder {
 public:
   DbpDecoder(buffer *buf) : buf(buf) {
     start = buf->start;
-    block_size = uleb_decode<uint32_t>(buf);
-    num_mb_pb = uleb_decode<uint32_t>(buf);
-    num_values = uleb_decode<uint32_t>(buf);
+    values_per_block = uleb_decode<uint32_t>(buf);
+    mini_blocks_per_block = uleb_decode<uint32_t>(buf);
+    total_value_count = uleb_decode<uint32_t>(buf);
     first_value = zigzag_decode<T, Tunsigned>(uleb_decode<Tunsigned>(buf));
-    mb_size = block_size / num_mb_pb;
-    // TODO: sanity check for values;
+    values_per_mini_block = values_per_block / mini_blocks_per_block;
+
+    // some sanity checks, ideally these should caught and re-thrown
+    // upstream with more information, e.g. file name and column name
+    if (values_per_block == 0) {
+      throw runtime_error(
+        "zero values per block is not allowed in "
+        "DELTA_BINARY_PACKED column"
+      );
+    }
+    if (values_per_block % 128 != 0) {
+      throw runtime_error(
+        "the number of values in a block must be multiple of 128, but it's " +
+        std::to_string(values_per_block)
+      );
+    }
+    if (mini_blocks_per_block == 0) {
+      throw runtime_error(
+        "zero miniblocks per block is not allowsd in "
+        "DELTA_BIANRY_PACKED column"
+      );
+    }
+    if (values_per_mini_block % 32 != 0) {
+      throw runtime_error(
+        "the number of values in a miniblock must be multiple of 32, but it's " +
+        std::to_string(values_per_mini_block)
+      );
+    }
+
   }
 
   inline uint32_t size() {
-    return num_values;
+    return total_value_count;
   }
 
   inline void decode(T *values) {
-    if (num_values == 0) return;
+    if (total_value_count == 0) return;
     values[0] = first_value;
     values++;
-    uint64_t todo = num_values - 1;
+    uint64_t todo = total_value_count - 1;
     while (todo > 0) {
       // start of a block
       T min_delta = zigzag_decode<T, Tunsigned>(uleb_decode<Tunsigned>(buf));
-      if (buf->len < num_mb_pb) {
+      if (buf->len < mini_blocks_per_block) {
         throw runtime_error("End of buffer while DBP decoding");
       }
-      vector<uint8_t> bit_widths(num_mb_pb);
-      memcpy(bit_widths.data(), buf->start, num_mb_pb);
-      buf->start += num_mb_pb; buf->len -= num_mb_pb;
-      for (auto i = 0; todo > 0 && i < num_mb_pb; i++) {
+      vector<uint8_t> bit_widths(mini_blocks_per_block);
+      memcpy(bit_widths.data(), buf->start, mini_blocks_per_block);
+      buf->start += mini_blocks_per_block; buf->len -= mini_blocks_per_block;
+      for (auto i = 0; todo > 0 && i < mini_blocks_per_block; i++) {
         // start of a miniblock
         int8_t bw = bit_widths[i];
-        uint64_t mb_vals = mb_size > todo ? todo : mb_size;
+        uint64_t mb_vals = values_per_mini_block > todo ? todo : values_per_mini_block;
         uint64_t mb_len = bw * mb_vals / 8 + ((bw * mb_vals) % 8 > 0);
         if (buf->len < mb_len) {
           throw runtime_error("End of buffer while DBP decoding");
@@ -66,9 +93,9 @@ public:
 private:
   struct buffer *buf;
   uint8_t *start;
-  uint32_t block_size;
-  uint32_t num_mb_pb;
-  uint32_t num_values;
-  uint32_t mb_size;
+  uint32_t values_per_block;
+  uint32_t mini_blocks_per_block;
+  uint32_t total_value_count;
+  uint32_t values_per_mini_block;
   T first_value;
 };
