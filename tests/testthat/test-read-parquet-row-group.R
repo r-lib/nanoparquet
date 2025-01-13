ec17c5e64f1bffa000e51986b9a8e59a504e6ff7
Gábor Csárdi <csardi.gabor@gmail.com>
2025-01-13 13:21:16 +0100

    Simplify read_parquet_row_group() tests

--- tests/testthat/test-read-parquet-row-group.R
+++ tests/testthat/test-read-parquet-row-group.R
@@ -2,8 +2,9 @@ test_that("plain and simple", {
   tmp <- tempfile(fileext = ".parquet")
   on.exit(unlink(tmp), add = TRUE)
 
+  df <- as.data.frame(test_df())
   write_parquet(
-    test_df(),
+    df,
     tmp,
     row_groups = c(1L, 11L, 21L, 31L),
     encoding = "PLAIN"
@@ -12,20 +13,25 @@ test_that("plain and simple", {
   # 4 row groups
   expect_snapshot(as.data.frame(read_parquet_metadata(tmp)$row_groups)[-1])
 
-  expect_snapshot({
-    as.data.frame(read_parquet_row_group(tmp, 0L))
-    as.data.frame(read_parquet_row_group(tmp, 1L))
-    as.data.frame(read_parquet_row_group(tmp, 2L))
-    as.data.frame(read_parquet_row_group(tmp, 3L))
-  })
+  un <- function(x) {
+    x <- as.data.frame(x)
+    rownames(x) <- NULL
+    x
+  }
+
+  expect_equal(un(read_parquet_row_group(tmp, 0L)), un(df[1:10,]))
+  expect_equal(un(read_parquet_row_group(tmp, 1L)), un(df[11:20,]))
+  expect_equal(un(read_parquet_row_group(tmp, 2L)), un(df[21:30,]))
+  expect_equal(un(read_parquet_row_group(tmp, 3L)), un(df[31:32,]))
 })
 
 test_that("dicts", {
   tmp <- tempfile(fileext = ".parquet")
   on.exit(unlink(tmp), add = TRUE)
 
+  df <- test_df()
   write_parquet(
-    test_df(),
+    df,
     tmp,
     row_groups = c(1L, 11L, 21L, 31L),
     encoding = c(large = "RLE", "RLE_DICTIONARY")
@@ -36,10 +42,14 @@ test_that("dicts", {
     as.data.frame(read_parquet_metadata(tmp)$row_groups)[-1]
   })
 
-  expect_snapshot({
-    as.data.frame(read_parquet_row_group(tmp, 0L))
-    as.data.frame(read_parquet_row_group(tmp, 1L))
-    as.data.frame(read_parquet_row_group(tmp, 2L))
-    as.data.frame(read_parquet_row_group(tmp, 3L))
-  })
+  un <- function(x) {
+    x <- as.data.frame(x)
+    rownames(x) <- NULL
+    x
+  }
+
+  expect_equal(un(read_parquet_row_group(tmp, 0L)), un(df[1:10,]))
+  expect_equal(un(read_parquet_row_group(tmp, 1L)), un(df[11:20,]))
+  expect_equal(un(read_parquet_row_group(tmp, 2L)), un(df[21:30,]))
+  expect_equal(un(read_parquet_row_group(tmp, 3L)), un(df[31:32,]))
 })
