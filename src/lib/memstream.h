a1588309e67e71e3add50951206625d8e35b3962
Gábor Csárdi <csardi.gabor@gmail.com>
2025-01-29 15:09:25 +0100

    Include <cstdint>
    To fix #100 again.

--- src/lib/memstream.h
+++ src/lib/memstream.h
@@ -4,6 +4,7 @@
 #include <memory>
 #include <cstring>
 #include <vector>
+#include <cstdint>
 
 // A growing buffer that can be used as an output stream.
 // It stores that data in an array of buffers, each bigger than
