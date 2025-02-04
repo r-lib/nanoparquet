c32b1b81cac864ca7a89eb0482f497e845dcc2d7
Gábor Csárdi <csardi.gabor@gmail.com>
2025-02-04 18:09:48 +0100

    Use the progress reporter
    So we actually see the errors.

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,5 +2,5 @@
 if (.Platform$r_arch == "i386" && .Platform$OS.type == "windows") {
   message("Skipping tests on Windows i386")
 } else {
-  testthat::test_check("nanoparquet", reporter = "location")
+  testthat::test_check("nanoparquet", reporter = "progress")
 }
